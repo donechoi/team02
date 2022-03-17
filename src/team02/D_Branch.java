f162c88f63c8fe0cb472b9fd9fa46cd64731e649
hongyerin31 <ghdhong2002@naver.com>
2022-03-17 11:40:40 +0900

    d_branch 작업했다.

---
+++ src/team02/D_Branch.java
@@ -0,0 +1,7 @@
+package team02;
+
+public class D_Branch {
+	public static void main(String[] args) {
+		System.out.println("d_branch는 작업했다.");
+	}
+}
--- src/team02/New_Prj.java
+++ src/team02/New_Prj.java
@@ -5,6 +5,7 @@ public class New_Prj {
 	public static void main(String[] args) {
 
 		System.out.println("공통파일 추가");
+		System.out.println("d_branch 작업함");
 	}
 
 }
