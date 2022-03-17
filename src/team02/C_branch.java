43b43e8d23f2a26d5a04165b253b4f025ea0ecf7
YD <fkrtn123@naver.com>
2022-03-17 11:40:29 +0900

    c_branch 작업했다.

---
+++ src/team02/C_branch.java
@@ -0,0 +1,8 @@
+package team02;
+
+public class C_branch {
+	public static void main(String[] args) {
+		
+		System.out.println("c_branch는 작업했다.");
+	}
+}
--- src/team02/New_Prj.java
+++ src/team02/New_Prj.java
@@ -5,6 +5,8 @@ public class New_Prj {
 	public static void main(String[] args) {
 
 		System.out.println("공통파일 추가");
+		System.out.println("c_branch 작업했다");
+		
 	}
 
 }
